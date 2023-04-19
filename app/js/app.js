34df06e43460c1542b4f7c0da1a791cb96cee07a
Syrone <cazonove@gmail.com>
2023-04-19 20:40:24 +0300

    build for view

--- app/js/app.js
+++ app/js/app.js
@@ -3,21 +3,27 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	const addEventOnElements = function(elements, eventType, callback) {
-		for(let i = 0, len = elements.length; i < len; i++) {
+	const addEventOnElements = function (elements, eventType, callback) {
+		for (let i = 0, len = elements.length; i < len; i++) {
 			elements[i].addEventListener(eventType, callback)
 		}
 	}
 
 	// Navbar toggler for mobile
 	const navbar = document.querySelector('[data-navbar]'),
-			navTogglers = document.querySelectorAll('[data-nav-toggler]'),
-			overlay = document.querySelector('[data-overlay]')
+		navTogglers = document.querySelectorAll('[data-nav-toggler]'),
+		overlay = document.querySelector('[data-overlay]')
 
-	const toggleNav = function() {
-		navbar.classList.toggle('active')
-		overlay.classList.toggle('active')
-		document.body.classList.toggle('nav-active')
+	const toggleNav = function () {
+		if (overlay.classList.contains('active')) {
+			overlay.classList.remove('active')
+			navbar.classList.remove('active')
+			document.body.classList.remove('nav-active')
+		} else {
+			overlay.classList.add('active')
+			navbar.classList.add('active')
+			document.body.classList.add('nav-active')
+		}
 	}
 
 	addEventOnElements(navTogglers, 'click', toggleNav)
@@ -26,9 +32,46 @@ document.addEventListener('DOMContentLoaded', () => {
 	const scrollHeader = () => {
 		const header = document.querySelector('[data-header]')
 		// When the scroll is greater than 50 viewport height, add the .scroll-header
-		globalThis.scrollY >= 50  ? header.classList.add('active')
-								  : header.classList.remove('active')
+		globalThis.scrollY >= 50 ? header.classList.add('active')
+			: header.classList.remove('active')
 	}
 	window.addEventListener('scroll', scrollHeader)
+
+	//CALLING THE REGISTRATION FORM
+	const signupForm = document.querySelector('[data-form-signup]'),
+		loginForm = document.querySelector('[data-form-login]'),
+		loginLink = document.querySelector('[data-login-link]'),
+		signLink = document.querySelector('[data-signup-link]'),
+		signupBtn = document.querySelectorAll('[data-btn-signup]'),
+		closeBtn = document.querySelectorAll('[data-btn-close]')
+
+	const openForm = function () {
+		overlay.classList.add('active')
+		signupForm.classList.add('active')
+		document.body.classList.add('nav-active')
+	}
+
+	const closeForm = function() {
+		overlay.classList.remove('active')
+		signupForm.classList.remove('active')
+		loginForm.classList.remove('active')
+		document.body.classList.remove('nav-active')
+	}
+
+	loginLink.addEventListener('click', () => {
+		signupForm.classList.remove('active')
+		loginForm.classList.add('active')
+	})
+
+	signLink.addEventListener('click', () => {
+		loginForm.classList.remove('active')
+		signupForm.classList.add('active')
+	})
+	
+	addEventOnElements(signupBtn, 'click', openForm)
+	addEventOnElements(closeBtn, 'click', closeForm)
+	overlay.addEventListener('click', closeForm)
+	
+
 
 })
